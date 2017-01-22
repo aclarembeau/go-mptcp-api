b98317f2ee75bca952998c65832c9958e912affc
Clarembeau Alexis <alexis.clarembeau@gmail.com>
2017-01-22 19:05:19 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,234 @@
+// Experimental Go binding for the multipath-tcp api
+// Based on the internet draft of B. Hesmans and O. Bonaventure
+// from https://tools.ietf.org/html/draft-hesmans-mptcp-socket-00
+package mptcp
+
+// #include "mptcp.h"
+// #include <stdlib.h>
+import "C"
+
+import "github.com/jbenet/go-sockaddr/net"
+import "github.com/jbenet/go-sockaddr"
+import (
+	"fmt"
+	"net"
+	"syscall"
+	"unsafe"
+	"errors"
+	"strconv"
+)
+
+// --- Helper unexported functions -------------------------------------------------------------------------------------
+
+// Translate a errno value to a fancy error message
+func getError(functName string, errno int) error {
+	switch errno {
+	case 0:
+		return nil
+	case C.EOPNOTSUPP:
+		return fmt.Errorf("%s: <errno %d> (operation not supported)", functName, errno)
+	case C.EINVAL:
+		return fmt.Errorf("%s: <errno %d> (invalid subflow id)", functName, errno)
+	case C.ENOPROTOOPT:
+		return fmt.Errorf("%s: <errno %d> (this option is unknown at the level specified)", functName, errno)
+	default:
+		return fmt.Errorf("%s: <errno %d>", functName, errno)
+	}
+}
+
+// Extracts the file descriptor from a TCPConn (and also sets the fd blocking)
+func getSockFd(conn *net.TCPConn) (int, error) {
+	if conn == nil {
+		return 0, errors.New("getSockFd: connection is nil")
+	}
+
+	fileinfo, fileerror := conn.File()
+	if fileerror != nil {
+		return 0, fileerror
+	}
+	sockfd := int(uint(fileinfo.Fd()))
+	return sockfd, nil
+}
+
+// Resoves a host name to a sockaddr_any structure (and socklen value)
+func resolveToSockaddr(host string) (*C.struct_sockaddr_any, int, error) {
+	ip, err := net.ResolveIPAddr("ip", host)
+	if err != nil {
+		return nil, 0, err
+	}
+	sockaddrRaw := sockaddrnet.IPAddrToSockaddr(ip)
+	sockaddrAny, socklen, _ := sockaddr.SockaddrToAny(sockaddrRaw)
+	sockaddrC := sockaddr.AnyToCAny(sockaddrAny)
+
+	return sockaddrC, int(socklen), err
+}
+
+// --- Library exported functions --------------------------------------------------------------------------------------
+
+// Get all the subflows opened in the current TCP connection. Returns a list of tuples (id, priority).
+// return = [[id subflow 1, priority subflow 1], [id subflow 2, priority subflow 2], ... ]
+func GetSubIDS(conn *net.TCPConn) ([][]int, error) {
+	// get the socket file descriptor
+	sockfd, fdErr := getSockFd(conn)
+	if fdErr != nil {
+		return nil, fmt.Errorf("getSubflows: (extracting fd) %v", fdErr)
+	}
+	defer syscall.SetNonblock(sockfd, true) // Restablishing non-blocking property
+
+	// get the subflow list pointer
+	subflowsInfo := C.getSubflows(C.int(sockfd))
+	nSubflows := int(subflowsInfo.resultCount)
+	ptrSubflows := unsafe.Pointer(subflowsInfo.resultPtr)
+
+	// handle the output
+	err := getError("getSubflows", int(subflowsInfo.errnoValue))
+	if err != nil {
+		return nil, err
+	}
+
+	// convert everything to a go int[][]
+	slice := (*[100]C.struct_mptcp_sub_status)(ptrSubflows)[:nSubflows:nSubflows]
+
+	sliceGo := make([][]int, len(slice))
+	var i int
+	for i = 0; i < nSubflows; i++ {
+		sliceGo[i] = []int{int(slice[i].id), -1}
+	}
+
+	// defer standard C free to old structure
+	defer C.free(unsafe.Pointer(subflowsInfo.globalStructureTofree)) // Transmitted C-to-go #1: freed
+
+	// and return the result
+	return sliceGo, nil
+}
+
+// Inspect a subflow. Takes the ID of a subflow in the subId parameter. Returns two string representing local
+// and distant endpoints using the host:port syntax.
+func GetSubTuple(conn *net.TCPConn, subId int) (string, string, error) {
+	// get the socket file descriptor
+	sockfd, fdErr := getSockFd(conn)
+	if fdErr != nil {
+		return "","", fmt.Errorf("inspectSubflow: (extracting fd) %v", fdErr)
+	}
+	defer syscall.SetNonblock(sockfd, true) // Restablishing non-blocking property
+
+	// call the c function to inspect a subflow from the mptcp library
+	subflowInspect := C.inspectSubflow(C.int(sockfd), C.int(subId))
+
+	err := getError("inspectSubflow", int(subflowInspect.errnoValue))
+	if err != nil {
+		return "","", err
+	}
+
+	localHostString := C.GoString(subflowInspect.localHost)
+	distantHostString := C.GoString(subflowInspect.distantHost)
+	defer C.free(unsafe.Pointer(subflowInspect.localHost))   // Transmitted C-to-go #2: freed
+	defer C.free(unsafe.Pointer(subflowInspect.distantHost)) // Transmitted C-to-go #3: freed
+
+	return net.JoinHostPort(localHostString, strconv.Itoa(int(subflowInspect.localPort))),
+		net.JoinHostPort(distantHostString, strconv.Itoa(int(subflowInspect.distantPort))), nil
+}
+
+// Close a specific subflow. The parameter subId is used to indicate the subflow and the 'how' argument
+// indicates how to close the subflow (this parameter is currently not used)
+func CloseSub(conn *net.TCPConn, subId int, how int) error {
+	// get the socket file descriptor
+	sockfd, fdErr := getSockFd(conn)
+	if fdErr != nil {
+		return fmt.Errorf("closeSubflow: (extracting fd) %v", fdErr)
+	}
+	defer syscall.SetNonblock(sockfd, true) // Restablishing non-blocking property
+
+	// call the c function to close a subflow from the mptcp library
+	cResultErrno := C.closeSubflow(C.int(sockfd), C.int(subId), C.int(how))
+
+	// make fancy output depending on the error code
+	return getError("closeSubflow", int(cResultErrno))
+}
+
+// Setsockopt for subflows. Takes as parameter the ID of the subflow, the level of the option, the option
+// type (integer) and the option value.
+func SetSubSockoptInt(conn *net.TCPConn, subId int, level int, opt int, val int) error {
+	// get the socket file descriptor
+	sockfd, fdErr := getSockFd(conn)
+	if fdErr != nil {
+		return fmt.Errorf("getSubflowSockoptInt: (extracting fd) %v", fdErr)
+	}
+	defer syscall.SetNonblock(sockfd, true) // Restablishing non-blocking property
+
+	// call the c function to add a subflow from the mptcp library
+	cResultErrno := C.setSubSockoptInt(C.int(sockfd), C.int(subId), C.int(level), C.int(opt), C.int(val))
+
+	return getError("setSubflowSockoptInt", int(cResultErrno))
+}
+
+// Getsockopt for subflows. Takes as parameter the ID of the subflow, the level of the option and the option
+// type
+func GetSubSockoptInt(conn *net.TCPConn, subId int, level int, opt int) (int, error) {
+	// get the socket file descriptor
+	sockfd, fdErr := getSockFd(conn)
+	if fdErr != nil {
+		return 0, fmt.Errorf("getSubflowSockoptInt: (extracting fd) %v", fdErr)
+	}
+	defer syscall.SetNonblock(sockfd, true) // Restablishing non-blocking property
+
+	// call the c function to add a subflow from the mptcp library
+	cResult := C.getSubSockoptInt(C.int(sockfd), C.int(subId), C.int(level), C.int(opt))
+
+	// make fancy output depending on the error code
+
+	err := getError("getSubflowSockoptInt", int(cResult.errnoValue))
+	if err == nil {
+		return int(cResult.result), nil
+	} else {
+		return 0, err
+	}
+}
+
+// Opens a new subflow specified by its local and distant endpoint (in the host:port format)
+func OpenSub(conn *net.TCPConn, localEndpoint string, distantEndpoint string)(int, error){
+	sourceHost, sourcePortString, errLocal := net.SplitHostPort(localEndpoint)
+	destHost, destPortString, errDistant := net.SplitHostPort(distantEndpoint)
+
+	if errLocal != nil {
+		return 0, fmt.Errorf("openSubflow: (splitting sourcehost:sourceport) %v", errLocal)
+	}
+	if errDistant != nil {
+		return 0, fmt.Errorf("openSubflow: (splitting desthost:destport) %v", errDistant)
+	}
+
+	sourcePort, sourcePortErr := strconv.Atoi(sourcePortString)
+	destPort, destPortErr := strconv.Atoi(destPortString)
+
+	if sourcePortErr != nil {
+		return 0, fmt.Errorf("openSubflow: (getting sourceport) %v", sourcePortErr)
+	}
+	if destPortErr != nil {
+		return 0, fmt.Errorf("openSubflow: (getting destport) %v", destPortErr)
+	}
+
+	// get the socket file descriptor
+	sockfd, fdErr := getSockFd(conn)
+	if fdErr != nil {
+		return 0, fmt.Errorf("openSubflow: (extracting fd) %v", fdErr)
+	}
+	defer syscall.SetNonblock(sockfd, true) // Restablishing non-blocking property
+
+	sourceSockaddr, sourceSocklen, sourceError := resolveToSockaddr(sourceHost)
+	if sourceError != nil {
+		return 0, fmt.Errorf("openSubflow: (resolving source) %v", sourceError)
+	}
+	destSockaddr, destSocklen, destError := resolveToSockaddr(destHost)
+	if destError != nil {
+		return 0, fmt.Errorf("openSubflow: (resolving destination) %v", destError)
+	}
+
+	// call the c function to add a subflow from the mptcp library
+	structResult := C.openSubflow(
+		C.int(sockfd),
+		unsafe.Pointer(sourceSockaddr), C.int(sourceSocklen), C.int(sourcePort),
+		unsafe.Pointer(destSockaddr), C.int(destSocklen), C.int(destPort))
+
+	// make fancy output depending on the error code
+	return int(structResult.flowId), getError("openSubflow", int(structResult.errnoValue))
+}
---
+++ main_test.go
@@ -0,0 +1,294 @@
+package mptcp
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"syscall"
+	"testing"
+	"reflect"
+	"time"
+)
+
+var SOURCE_HOST string = "192.168.33.10"
+var DEST_HOST string = "130.104.230.45"
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Function used to establish a connection from sourceH:sourceP to destH:destP
+// Fatal fail if the connection couldn't be established
+func establishConnection(t *testing.T, sourceH string, sourceP string, destH string, destP string) *net.TCPConn {
+	localAddr, _ := net.ResolveTCPAddr("tcp", net.JoinHostPort(sourceH, sourceP))
+	distantAddr, _ := net.ResolveTCPAddr("tcp", net.JoinHostPort(destH, destP))
+	conn, errCon := net.DialTCP("tcp", localAddr, distantAddr)
+	if errCon != nil {
+		t.Fatalf("Fatal error: unable to connect because %v", errCon)
+	}
+
+	return conn
+}
+
+// Logs a message depending on the result of the `passCondition` boolean variable.
+// Shows "test OK" or a fancy error message indicating the excepted and the value we got
+// in case of failure
+func testExcepted(t *testing.T, passCondition bool, testName string, excepted string, got string) {
+	if !passCondition {
+		t.Errorf("[X] %s: excepted = %s, got = %s", testName, excepted, got)
+	} else {
+		t.Logf("[ ] %s: test OK!", testName)
+	}
+}
+
+// Waits for 300 ms. This function is mainly used after opening or closing a subflow to wait
+// for the operation to complete
+func waitABit(){
+	time.Sleep(300 * time.Millisecond)
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Tests the function used to open the subflows
+func TestOpenSubflow(t *testing.T) {
+	// Establish the connection
+
+	conn := establishConnection(t, SOURCE_HOST, "64100", DEST_HOST, "80")
+	defer conn.Close()
+
+	// Part 1: Opening 3 subflows and checking validity
+
+	t.Log("Opening three valid subflows")
+	fmt.Fprint(conn, "GET / HTTP/1.1\r\n")
+	openedId1, openedError1 := OpenSub(conn, net.JoinHostPort(SOURCE_HOST, "64101"), net.JoinHostPort(DEST_HOST, "80"))
+	openedId2, openedError2 := OpenSub(conn, net.JoinHostPort("", "64102"), net.JoinHostPort(DEST_HOST, "80"))
+	openedId3, openedError3 := OpenSub(conn, net.JoinHostPort(SOURCE_HOST, "0"), net.JoinHostPort(DEST_HOST, "80"))
+	waitABit()
+
+	t.Log("Checking result")
+	testExcepted(t, openedId1 == 2 && openedId2 == 3 && openedId3 == 4,
+		"openedId value",
+		"(2 3 4)",
+		fmt.Sprintf("(%d %d %d)", openedId1, openedId2, openedId3))
+
+	testExcepted(t, openedError1 == nil, "opening standard subflow",
+		"success", fmt.Sprintf("error '%v'", openedError1))
+	testExcepted(t, openedError2 == nil, "opening any source address subflow",
+		"success", fmt.Sprintf("error '%v'", openedError2))
+	testExcepted(t, openedError3 == nil, "opening any source port subflow",
+		"success", fmt.Sprintf("error '%v'", openedError3))
+
+	// Part 2 : Opening 3 erroneous subflows and checking the error
+
+	t.Log("Opening three invalid subflows")
+	_, err2 := OpenSub(nil, net.JoinHostPort("", "64105"), net.JoinHostPort(DEST_HOST, "80"))
+	_, err3 := OpenSub(conn, net.JoinHostPort("", "20"), net.JoinHostPort(DEST_HOST, "80"))
+	_, err4 := OpenSub(conn, net.JoinHostPort("", "64100"), net.JoinHostPort(DEST_HOST, "80"))
+
+	t.Log("Checking result")
+	testExcepted(t, err2 != nil, "opening nil-connection subflow", "failure", "success")
+	testExcepted(t, err3 != nil, "opening source port < 1024 subflow", "failure", "success")
+	testExcepted(t, err4 != nil, "opening already used subflow", "failure", "success")
+}
+
+// Tests the function used to get information about the subflow tuple
+func TestInspectSubflow(t *testing.T) {
+	// Establish the connection
+
+	conn := establishConnection(t, SOURCE_HOST, "64200", DEST_HOST, "80")
+	defer conn.Close()
+
+	// Part 1: Inspect the main subflow
+
+	t.Log("Inspecting subflow #1")
+	fmt.Fprint(conn, "GET / HTTP/1.1\r\n")
+	source1, dest1, inspectErr1 := GetSubTuple(conn, 1)
+
+	testExcepted(t, inspectErr1 == nil, "inspection status", "success", fmt.Sprintf("error '%v'", inspectErr1))
+	testExcepted(t,
+		source1 == net.JoinHostPort(SOURCE_HOST, "64200") && dest1 == net.JoinHostPort(DEST_HOST, "80"),
+		"inspection result",
+		fmt.Sprintf("(%s %s)", net.JoinHostPort(SOURCE_HOST, "64200"), net.JoinHostPort(DEST_HOST, "80")),
+		fmt.Sprintf("(%s %s)", source1, dest1))
+
+	// Part 2: Open a new subflow and inspect it
+
+	t.Log("Opening new subflow")
+	_, openingError := OpenSub(conn, net.JoinHostPort("", "64201"), net.JoinHostPort(DEST_HOST, "80"))
+	waitABit()
+
+	testExcepted(t, openingError == nil, "opening new subflow", "success", fmt.Sprintf("error '%v'", openingError))
+
+	t.Log("Inspecting subflow #2")
+	source2, dest2, inspectErr2 := GetSubTuple(conn, 2)
+
+	testExcepted(t, inspectErr2 == nil, "inspection status", "success", fmt.Sprintf("error '%v'", inspectErr2))
+	testExcepted(t,
+		source2 == net.JoinHostPort(SOURCE_HOST, "64201") && dest2 == net.JoinHostPort(DEST_HOST, "80"),
+		"inspection result",
+		fmt.Sprintf("(%s %s)", net.JoinHostPort(SOURCE_HOST, "64201"), net.JoinHostPort(DEST_HOST, "80")),
+		fmt.Sprintf("(%s %s)", source2, dest2))
+
+	// Part 3: Inspecting non-existing subflows
+
+	t.Log("Inspecting invalid subflows")
+	_, _, inspectErr3 := GetSubTuple(conn, 0)
+	_, _, inspectErr4 := GetSubTuple(nil, 1)
+	_, _, inspectErr5 := GetSubTuple(conn, 50)
+
+	testExcepted(t, inspectErr3 != nil, "inspecting subflow #0", "failure", "success")
+	testExcepted(t, inspectErr4 != nil, "inspecting nil connection", "failure", "success")
+	testExcepted(t, inspectErr5 != nil, "inspecting subflow #50", "failure", "success")
+
+}
+
+// Tests the function used to remove the subflows
+func TestCloseSubflow(t *testing.T) {
+	// Establishing connection
+
+	conn := establishConnection(t, SOURCE_HOST, "64300", DEST_HOST, "80")
+	defer conn.Close()
+
+	// Part 1 : Opening then closing subflows
+
+	t.Log("Opening new subflow")
+	fmt.Fprint(conn, "GET / HTTP/1.1\r\n")
+	_, openingError := OpenSub(conn, net.JoinHostPort("", "64301"), net.JoinHostPort(DEST_HOST, "80"))
+	waitABit()
+
+	testExcepted(t, openingError == nil, "opening new subflow", "success", fmt.Sprintf("error '%v'", openingError))
+
+	t.Log("Closing some subflows")
+	closingErr1 := CloseSub(conn, 2, 0)
+	testExcepted(t, closingErr1 == nil, "closing subflow #2", "success", fmt.Sprintf("error '%v'", closingErr1))
+	closingErr2 := CloseSub(conn, 1, 0)
+	testExcepted(t, closingErr2 == nil, "closing subflow #1", "success", fmt.Sprintf("error '%v'", closingErr2))
+
+	// Part 2 : Closing non-existing subflow
+
+	closingErr3 := CloseSub(conn, 42, 0)
+	testExcepted(t, closingErr3 != nil, "closing subflow #45", "failure", "success")
+
+}
+
+func TestGetSubflows(t *testing.T) {
+	// Establishing connection
+
+	conn := establishConnection(t, SOURCE_HOST, "64400", DEST_HOST, "80")
+	defer conn.Close()
+
+	// Part 1 : Opening four subflows and getting the list of subflows
+
+	fmt.Fprint(conn, "GET / HTTP/1.1\r\n")
+
+	t.Log("Opening 4 subflows")
+	_, openingError1 := OpenSub(conn, net.JoinHostPort(SOURCE_HOST, "64401"), net.JoinHostPort(DEST_HOST, "80"))
+	_, openingError2 := OpenSub(conn, net.JoinHostPort(SOURCE_HOST, "64402"), net.JoinHostPort(DEST_HOST, "80"))
+	_, openingError3 := OpenSub(conn, net.JoinHostPort(SOURCE_HOST, "64403"), net.JoinHostPort(DEST_HOST, "80"))
+	_, openingError4 := OpenSub(conn, net.JoinHostPort(SOURCE_HOST, "64404"), net.JoinHostPort(DEST_HOST, "80"))
+	waitABit()
+
+	testExcepted(t, openingError1 == nil && openingError2 == nil && openingError3 == nil && openingError4 == nil,
+		"opening subflows",
+		"(nil, nil, nil, nil)",
+		fmt.Sprintf("(%v %v %v %v)", openingError1, openingError2, openingError3, openingError4))
+
+	t.Log("Listing subflows")
+	list, getError := GetSubIDS(conn)
+
+	testExcepted(t, getError == nil, "listing subflows", "success", fmt.Sprintf("error '%v'", getError))
+
+	ids := []int{list[0][0], list[1][0], list[2][0], list[3][0], list[4][0]}
+	prios := []int{list[0][1], list[1][1], list[2][1], list[3][1], list[4][1]}
+
+	min := ids[0]
+	for _, elem := range ids {
+		if elem < min {min = elem; }
+	}
+
+	for index, _ := range ids {
+		ids[index] -= min
+	}
+
+	testExcepted(t, reflect.DeepEqual([]int{4,3,2,1,0},ids), "checking subflow IDs - maxvalue", "4,3,2,1,0", fmt.Sprintf("%v", ids))
+	testExcepted(t, reflect.DeepEqual([]int{0,0,0,0,0} ,prios), "checking subflow priorities", "0,0,0,0,0", fmt.Sprintf("%v", ids))
+}
+
+func TestSubsockopt(t *testing.T) {
+	// Establishing the connection
+
+	conn := establishConnection(t, SOURCE_HOST, "61500", DEST_HOST, "80")
+	defer conn.Close()
+
+	// Get command
+	fmt.Fprint(conn, "GET / HTTP/1.1\r\n")
+	t.Log("Opening new subflow")
+	_, openingError := OpenSub(conn, net.JoinHostPort("", "61501"), net.JoinHostPort(DEST_HOST, "80"))
+	waitABit()
+
+	testExcepted(t, openingError == nil, "opening new subflow", "success", fmt.Sprintf("error '%v'", openingError))
+
+	// Part 1 : Setting an option
+
+	t.Log("Setting socket option")
+	settingError := SetSubSockoptInt(conn, 2, syscall.SOL_IP, syscall.IP_TOS, 28)
+	testExcepted(t, settingError == nil, "setting IP_TOS", "success", fmt.Sprintf("error '%v'", settingError))
+
+	// Part 2 : Getting the value of the newly set option
+
+	t.Log("Getting socket option")
+	valMeasured, gettingError := GetSubSockoptInt(conn, 2, syscall.SOL_IP, syscall.IP_TOS)
+	testExcepted(t, gettingError == nil, "getting IP_TOS", "success", fmt.Sprintf("error '%v'", gettingError))
+	testExcepted(t, valMeasured == 28, "getting IP_TOS (value)", "28", fmt.Sprintf("%d", valMeasured))
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Example about how to use the mptcp-api go binding. Works by trying to
+// connect to multipath-tcp.org. Then, opens, lists, deletes and get information
+// about the different subflows
+func Example() {
+	localAddr, _ := net.ResolveTCPAddr("tcp", net.JoinHostPort(SOURCE_HOST, "64001"))
+	distantAddr, _ := net.ResolveTCPAddr("tcp", net.JoinHostPort(DEST_HOST, "80"))
+	conn, _ := net.DialTCP("tcp", localAddr, distantAddr)
+	defer conn.Close()
+
+	// Get command
+	fmt.Fprint(conn, "GET  / HTTP/1.1\r\n")
+
+	// Opening 4 subflows
+	OpenSub(conn, net.JoinHostPort("", "64002"), net.JoinHostPort(DEST_HOST, "80"))
+	OpenSub(conn, net.JoinHostPort("", "64003"), net.JoinHostPort(DEST_HOST, "80"))
+	OpenSub(conn, net.JoinHostPort("", "64004"), net.JoinHostPort(DEST_HOST, "80"))
+	OpenSub(conn, net.JoinHostPort("", "64005"), net.JoinHostPort(DEST_HOST, "80"))
+	waitABit()
+
+	// Listing subflows
+	fmt.Println(GetSubIDS(conn))
+
+	// Closing one subflow
+	fmt.Println(CloseSub(conn, 2, 0))
+	waitABit()
+
+	// Checking effect of the modification
+	list, getError := GetSubIDS(conn)
+	fmt.Println(getError)
+	fmt.Println(list)
+
+	// Now, getting even more details (source, port) tuples
+	for _, element := range list {
+		subflowId := element[0]
+		fmt.Println(GetSubTuple(conn, subflowId))
+	}
+
+	// Setting an option
+	SetSubSockoptInt(conn, 3, syscall.SOL_IP, syscall.IP_TOS, 28)
+
+	// Getting the value
+	fmt.Println(GetSubSockoptInt(conn, 3, syscall.SOL_IP, syscall.IP_TOS))
+
+	// And checking that data are received
+	var buf bytes.Buffer
+	io.Copy(&buf, conn)
+	fmt.Println("Len of data received: ", buf.Len())
+
+}
